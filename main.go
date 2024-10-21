f6b1409acec7e2f0604dfb2d2cd846070390b88a
yuankang <troyyuan.smit@gmail.com>
2024-10-21 11:21:20 +0800

    can use ok

--- main.go
+++ main.go
@@ -3,57 +3,14 @@ package main
 import (
 	"crypto/md5"
 	"encoding/hex"
-	"fmt"
+	"flag"
 	"io"
-	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 )
 
-func main() {
-	//log.SetFlags(log.LstdFlags | log.Lshortfile)
-	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Lshortfile)
-	dir := "pic/"
-	//dir := "/Users/yuankang/Pictures/wallpaper"
-
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		log.Println("无法读取目录:", err)
-		return
-	}
-
-	//seen := make(map[string]bool)
-
-	for _, file := range files {
-		filePath := filepath.Join(dir, file.Name())
-
-		if isImageFile(filePath) {
-			//log.Println(filePath)
-
-			bs, err := GetPartialData(filePath)
-			if err != nil {
-				log.Println("无法读取文件:", err)
-				continue
-			}
-			//log.Printf("bs:%x", bs)
-
-			md5 := calculateMD5(bs)
-			log.Printf("fn:%s, md5:%x", file.Name(), md5)
-
-			/*
-				if !seen[hash] {
-					seen[hash] = true
-					log.Println("全新图片:", filePath)
-				} else {
-					log.Println("重复图片:", filePath)
-				}
-			*/
-		}
-	}
-}
-
-func isImageFile(filePath string) bool {
+func IsImageFile(filePath string) bool {
 	fe := filepath.Ext(filePath)
 	if fe == ".jpg" || fe == ".png" || fe == ".jpeg" || fe == ".bmp" || fe == ".gif" || fe == ".webp" {
 		return true
@@ -62,148 +19,115 @@ func isImageFile(filePath string) bool {
 	return false
 }
 
-func calculateMD5(data []byte) string {
-	hasher := md5.New()
-	hasher.Write(data)
-
-	hashInBytes := hasher.Sum(nil)
-	md5String := hex.EncodeToString(hashInBytes)
-	return md5String
-}
-
-func GetPartialData(filePath string) ([]byte, error) {
-	f100b, err := getFirstNBytes(filePath, 10)
+func FileMd5(fn string) (string, error) {
+	file, err := os.Open(fn)
 	if err != nil {
-		log.Println("Error reading first 100 bytes:", err)
-		return nil, err
-	}
-	//log.Printf("F100:%x", f100b)
-
-	l100b, err := getLastNBytes(filePath, 10)
-	if err != nil {
-		log.Println("Error reading last 100 bytes:", err)
-		return nil, err
-	}
-	//log.Printf("L100:%x", l100b)
-
-	bs := append(f100b, l100b...)
-	return bs, nil
-}
-
-//首部50字节+中间50字节+尾部50字节+文件长度
-func GetPartialData0(filePath string) ([]byte, error) {
-	f100b, err := getFirstNBytes(filePath, 10)
-	if err != nil {
-		log.Println("Error reading first 100 bytes:", err)
-		return nil, err
-	}
-	//log.Printf("F100:%x", f100b)
-
-	l100b, err := getLastNBytes(filePath, 10)
-	if err != nil {
-		log.Println("Error reading last 100 bytes:", err)
-		return nil, err
-	}
-	//log.Printf("L100:%x", l100b)
-
-	bs := append(f100b, l100b...)
-	return bs, nil
-}
-
-func getFirstNBytes(filePath string, n int) ([]byte, error) {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	buffer := make([]byte, n)
-	_, err = io.ReadFull(file, buffer)
-	if err != nil {
-		return nil, err
-	}
-
-	return buffer, nil
-}
-
-func getMiddleNBytes(filePath string, n int) ([]byte, error) {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	// 获取文件信息
-	fileInfo, err := file.Stat()
-	if err != nil {
-		return nil, err
-	}
-
-	// 确保文件大小至少为 n 字节
-	if fileInfo.Size() < int64(n) {
-		return nil, fmt.Errorf("文件太小，无法获取中间 %d 字节", n)
-	}
-
-	// 计算中间位置
-	middleOffset := fileInfo.Size()/2 - int64(n/2)
-
-	// 移动文件指针到中间位置
-	_, err = file.Seek(middleOffset, io.SeekStart)
-	if err != nil {
-		return nil, err
-	}
-
-	// 读取中间 n 字节的数据
-	middleBytes := make([]byte, n)
-	_, err = file.Read(middleBytes)
-	if err != nil {
-		return nil, err
-	}
-
-	return middleBytes, nil
-}
-
-func getLastNBytes(filePath string, n int) ([]byte, error) {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return nil, err
+		log.Println("Error opening file:", err)
+		return "", err
 	}
 	defer file.Close()
 
 	fileInfo, err := file.Stat()
 	if err != nil {
-		return nil, err
+		log.Println("Error getting file size:", err)
+		return "", err
 	}
-
 	fileSize := fileInfo.Size()
-	if fileSize < int64(n) {
-		n = int(fileSize)
+
+	// 定义读取位置
+	var start, middle, end int64
+	bufferSize := int64(100)
+
+	start = 0
+	middle = fileSize/2 - bufferSize/2
+	if middle < 0 {
+		middle = 0
+	}
+	end = fileSize - bufferSize
+	if end < 0 {
+		end = 0
 	}
 
-	buffer := make([]byte, n)
-	_, err = file.ReadAt(buffer, fileSize-int64(n))
-	if err != nil {
-		return nil, err
+	// 读取前100字节
+	startBuffer := make([]byte, bufferSize)
+	_, err = file.ReadAt(startBuffer, start)
+	if err != nil && err != io.EOF {
+		log.Println("Error reading start buffer:", err)
+		return "", err
 	}
 
-	return buffer, nil
+	// 读取中间100字节
+	middleBuffer := make([]byte, bufferSize)
+	_, err = file.ReadAt(middleBuffer, middle)
+	if err != nil && err != io.EOF {
+		log.Println("Error reading middle buffer:", err)
+		return "", err
+	}
+
+	// 读取后100字节
+	endBuffer := make([]byte, bufferSize)
+	_, err = file.ReadAt(endBuffer, end)
+	if err != nil && err != io.EOF {
+		log.Println("Error reading end buffer:", err)
+		return "", err
+	}
+
+	// 连接所有字节
+	data := append(startBuffer, middleBuffer...)
+	data = append(data, endBuffer...)
+
+	// 计算MD5值
+	md5hash := md5.Sum(data)
+	md5str := hex.EncodeToString(md5hash[:])
+	return md5str, nil
 }
 
-func getFileLength(filePath string) (int64, error) {
-	file, err := os.Open(filePath)
+// TraverseFolder 遍历指定文件夹并返回每个文件的全路径
+func TraverseFolder(folder string) ([]string, error) {
+	var files []string
+	err := filepath.Walk(folder, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+		return nil
+	})
+	return files, err
+}
+
+//1 启动时加载sqlite数据库中的所有数据
+//2 http下发任务, 指定检查路径
+//3 获取路径下的所有文件的全路径
+//4 循环计算每个文件的md5值
+//5 如果md5值不存在, 就入库
+//6 如果md5值存在, 就检查已存在md5值对应的文件是否存在
+//7 如果文件存在 就删除新md5值对应的文件
+//8 如果文件不存在, 就删除老md5的记录, 并入库新md5值的记录
+
+//go run main.go -folder /path/to/your-folder
+//./DelSameFile -folder ./pic/
+func main() {
+	//log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Lshortfile)
+
+	// 定义命令行参数
+	folder := flag.String("folder", ".", "folder to traverse")
+	flag.Parse()
+
+	files, err := TraverseFolder(*folder)
 	if err != nil {
-		return 0, err
-	}
-	defer file.Close()
-
-	// 获取文件信息
-	fileInfo, err := file.Stat()
-	if err != nil {
-		return 0, err
+		log.Fatalf("Error traversing folder: %v", err)
 	}
 
-	// 获取文件长度
-	fileLength := fileInfo.Size()
-
-	return fileLength, nil
+	for _, file := range files {
+		md5hash, err := FileMd5(file)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		log.Printf("md5:%s, file:%s", md5hash, file)
+	}
 }
